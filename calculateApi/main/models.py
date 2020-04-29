b1a594a0c6d9e1d971636c5757aa946f1c7e247c
nciganovic <nciganovic99@gmail.com>
2020-04-29 14:12:35 +0200

    ADD model added

--- calculateApi/main/models.py
+++ calculateApi/main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import int_list_validator
 
-# Create your models here.
+class Add(models.Model):
+    value = models.CharField(max_length=255, validators=[int_list_validator(sep=', ', message=None, code='invalid', allow_negative=False)]) 
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.value
