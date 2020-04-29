99a140fa04d014ef1dcd1148cf81c76ac48312a9
nciganovic <nciganovic99@gmail.com>
2020-04-29 19:41:57 +0200

    imporved AddViewSet class

--- calculateApi/main/views.py
+++ calculateApi/main/views.py
@@ -1,31 +1,39 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User, Group
 from django.http import HttpResponse
-from rest_framework import viewsets
-from rest_framework import permissions
-from main.serializer import UserSerializer, GroupSerializer, AddSerializer
+from django.core.validators import int_list_validator
+from django.core.exceptions import ValidationError
+from rest_framework import viewsets, permissions, generics
+from main.serializer import AddSerializer
 from main.models import Add
 
 class AddViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
-    #queryset = Add.objects.all()
     serializer_class = AddSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
-        """
-        This view should return a list of all the adds
-        for the currently authenticated user.
-        """
         this_user = self.request.user
         return Add.objects.filter(user=this_user)
 
     def create(self, validated_data):
-        value = request.POST['value']
-        print("VALUE => ", value)
-        return HttpResponse("Here's the text of the Web page.")
+        this_value = validated_data.POST['value']
+        validator = int_list_validator(sep=', ', message=None, code='invalid', allow_negative=False)
 
-AddViewSet.as_view({'get': 'list', 'post':'create'}) # For: /api/subscriber
-AddViewSet.as_view({'get': 'retrieve', 'put':'update'}) # For: /api/subscriber/1
+        try:
+            validator(this_value)
+
+            try: 
+                instance = Add.objects.get(user=self.request.user)
+                instance.value = instance.value + ", " + this_value
+                instance.save()
+            except:
+                add = Add()
+                add.value = this_value
+                add.user = self.request.user
+                add.save()
+                return HttpResponse("")
+            
+            return HttpResponse("")
+        except ValidationError as e:
+            print(e)
+            return HttpResponse("Invalid Data")
