5a23d93e3ae4ef52a07eae94b4f56f9749ab5b8c
nciganovic <nciganovic99@gmail.com>
2020-04-29 18:23:55 +0200

    remove unnecessery serializers

--- calculateApi/main/serializer.py
+++ calculateApi/main/serializer.py
@@ -3,16 +3,6 @@ from django.core.validators import int_list_validator
 from rest_framework import serializers
 from main.models import Add
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ['url', 'username', 'email', 'groups']
-
-class GroupSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Group
-        fields = ['url', 'name']
-
 class AddSerializer(serializers.ModelSerializer):
     class Meta:
         model = Add
