87d9ecf7db095fcec67eaa10aa89b68cb02ae1c2
nciganovic <nciganovic99@gmail.com>
2020-04-30 19:53:52 +0200

    History table added

--- calculateApi/main/admin.py
+++ calculateApi/main/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from main.models import Add, Calculate
+from main.models import Add, Calculate, History
 
 admin.site.register(Add)
 admin.site.register(Calculate)
+admin.site.register(History)
--- calculateApi/main/models.py
+++ calculateApi/main/models.py
@@ -8,6 +8,9 @@ class Add(models.Model):
     
     def __str__(self):
         return self.value
+    
+    class Meta:
+        verbose_name_plural = "Adds"
 
 class Calculate(models.Model):
     number = models.IntegerField() 
@@ -15,3 +18,17 @@ class Calculate(models.Model):
     
     def __str__(self):
         return str(self.number)
+    
+    class Meta:
+        verbose_name_plural = "Calculations"
+
+class History(models.Model):
+    array = models.CharField(max_length=255, validators=[int_list_validator(sep=', ', message=None, code='invalid', allow_negative=False)])
+    calculations = models.CharField(max_length=255, validators=[int_list_validator(sep=', ', message=None, code='invalid', allow_negative=False)])
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.id} - {self.user}'
+    
+    class Meta:
+        verbose_name_plural = "History"
