1911015ebdbb676b3290b6a0d77d41c6e085d3e1
nciganovic <nciganovic99@gmail.com>
2020-05-02 10:05:52 +0200

    history route tests added

--- calculateApi/main/tests.py
+++ calculateApi/main/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 from rest_framework.test import APITestCase, APIRequestFactory
 from requests.auth import HTTPBasicAuth
-from .models import Add, Calculate
+from .models import Add, Calculate, History
 from django.contrib.auth.models import User
 import json
 
@@ -85,6 +85,20 @@ class CalculateTestCase(APITestCase):
         Calculate.objects.filter(user=user).delete()
         response = self.client.get('/calculate/')
         self.assertEqual(response.content.decode('ascii'), "Numbers not provided.")
+
+    def test_fail_content_all(self):
+        user = User.objects.get(username="test")
+        Add.objects.get(user=user).delete()
+        Calculate.objects.filter(user=user).delete()
+        response = self.client.get('/calculate/?all=1')
+        self.assertEqual(response.content.decode('ascii'), "Numbers not provided.")
+
+    def test_fail_statuscode_all(self):
+        user = User.objects.get(username="test")
+        Add.objects.get(user=user).delete()
+        Calculate.objects.filter(user=user).delete()
+        response = self.client.get('/calculate/?all=1')
+        self.assertEqual(response.status_code, 406)
         
 
 class ResetTestCase(APITestCase):
@@ -117,4 +131,69 @@ class ResetTestCase(APITestCase):
         Calculate.objects.filter(user=user).delete()
         response = self.client.post('/reset/')
         self.assertEqual(response.content.decode('ascii'), "You dont have any calculations yet.")
+
+class HistoryTestCase(APITestCase):
+    def setUp(self):
+        User = get_user_model()
+        user = User.objects.create_user('test', 'test@gmail.com', 'test123')
+        self.client.login(username='test', password='test123')
+        
+        #First calculation
+        response = self.client.post('/add/', {'value': '3, 5'})
+        response = self.client.get('/calculate/')
+        response = self.client.post('/add/', {'value': '8'})
+        response = self.client.get('/calculate/')
+        response = self.client.post('/add/', {'value': '2'})
+        response = self.client.get('/calculate/')
+        
+        response = self.client.post('/reset/')
+
+        #Second calculation
+        response = self.client.post('/add/', {'value': '2'})
+        response = self.client.get('/calculate/')
+        response = self.client.post('/add/', {'value': '4, 6'})
+        response = self.client.get('/calculate/')
+        response = self.client.post('/add/', {'value': '8'})
+        response = self.client.get('/calculate/')
+
+        response = self.client.post('/reset/')
+        
+    def test_content(self):
+        response = self.client.get('/history/')
+        json_response = '[{"id": 2, "array": [2, 4, 6, 8], "calculations": [2, 12, 20]}, {"id": 1, "array": [3, 5, 8, 2], "calculations": [8, 16, 18]}]'
+        self.assertEqual(response.content.decode('ascii'), json_response)
+
+    def test_statuscode(self):
+        response = self.client.get('/history/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_content_id(self):
+        response = self.client.get('/history/?id=2')
+        json_response = '[{"id": 2, "array": [2, 4, 6, 8], "calculations": [2, 12, 20]}]'
+        self.assertEqual(response.content.decode('ascii'), json_response)
+
+    def test_statuscode_id(self):
+        response = self.client.get('/history/?id=2')
+        json_response = '[{"id": 2, "array": [2, 4, 6, 8], "calculations": [2, 12, 20]}]'
+        self.assertEqual(response.status_code, 200)
+
+    def test_fail_content(self):
+        user = User.objects.get(username="test")
+        History.objects.filter(user=user).delete()
+        response = self.client.get('/history/')
+        self.assertEqual(response.content.decode('ascii'), "History is empty")
+
+    def test_fail_statuscode(self):
+        user = User.objects.get(username="test")
+        History.objects.filter(user=user).delete()
+        response = self.client.get('/history/')
+        self.assertEqual(response.status_code, 404)
+
+    def test_fail_content_id(self):
+        response = self.client.get('/history/?id=3')
+        self.assertEqual(response.content.decode('ascii'), "This element does not exist")
+
+    def test_fail_statuscode_id(self):
+        response = self.client.get('/history/?id=3')
+        self.assertEqual(response.status_code, 404)
 
